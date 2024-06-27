99842fce2e598c15646f9f8cceab1e8cadef161e
mp5650 <71712126+mp5650@users.noreply.github.com>
2024-06-27 15:44:09 -0400

    Create DipoleForces.py

---
+++ DipoleForces.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+dM1 = [1,1]
+dP1 = [1,1]
+dM2 = [-1,1]
+dP2 = [-1,1]
+dM3 = [-1,-1]
+dP3 = [-1,-1]
+dM4 = [1,-1]
+dP4 = [1,-1]
+
+def DipoleForce(dipoleMoment1, dipolePosition1, dipoleMoment2, dipolePosition2):
+    m1 = np.array(dipoleMoment1)
+    m2 = np.array(dipoleMoment2)
+    R = np.array(dipolePosition2) - np.array(dipolePosition1)
+    mag_R = np.sqrt(R.dot(R))
+    coefficient =  ((3*pow(10,-7))/pow(mag_R,5))
+    first_term = m1.dot(R)*m2
+    second_term = m2.dot(R)*m1
+    third_term = m1.dot(m2)*R
+    fourth_term = R* (5* m1.dot(R) * m2.dot(R))/(pow(mag_R,2))
+    return coefficient * (first_term + second_term + third_term - fourth_term)
+
+print(DipoleForce(dM1,dP1,dM2,dP2)+DipoleForce(dM1,dP1,dM3,dP3)+DipoleForce(dM1,dP1,dM4,dP4))
